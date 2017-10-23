a7259b67ff716607324104a83e0682721f555e57
theUnspecified <222mzb@gmail.com>
2017-10-24 02:05:40 +0600

    initial approach

--- practice 2.py
+++ practice 2.py
@@ -1,8 +1,10 @@
 import tkinter   as tk
 import os
 import numpy
-LARGE_FONT = ("Verdana", 12)
+import cv2
 
+LARGE_FONT = ("Verdana", 12)
+im = cv2.imread('1.jpg')
 
 class SeaofBTCapp(tk.Tk):
     def __init__(self, *args, **kwargs):
@@ -22,6 +24,7 @@ class SeaofBTCapp(tk.Tk):
 
         frame.grid(row=0, column=0, sticky="nsew")
 
+
         self.show_frame(StartPage)
 
     def show_frame(self, cont):
@@ -29,15 +32,19 @@ class SeaofBTCapp(tk.Tk):
         frame.tkraise()
 
 def qf():
-    print("done")
+    cv2.imshow("images", im)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 class StartPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="Start Page", font=LARGE_FONT)
-        label.pack(pady=10, padx=10)
+        label.pack(pady=100, padx=100)
 
         button1 = tk.Button(self, text = "show plate", command =qf )
-        # button12 = tk.Button(self, text = "show number", command = qf)
+        button2 = tk.Button(self, text = "show number", command = qf)
+        button1.pack()
+        button2.pack()
 
 
 app = SeaofBTCapp()
